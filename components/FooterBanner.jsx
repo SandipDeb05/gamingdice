bb35735925d90358a7191877ee1bdd9eaf8334c4
SandipDeb <sandip.deb@xopuntech.com>
2022-05-20 21:02:21 +0530

    footer banner styling done

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -25,15 +25,19 @@ const FooterBanner = ({
           <h3>{largeText2}</h3>
           <p>{saleTime}</p>
         </div>
+        <div>
+          <img src={urlFor(image)} alt="" className="footer-banner-image" />
+        </div>
         <div className="right">
           <p>{smallText}</p>
           <h3>{midText}</h3>
           <p>{desc}</p>
           <Link href={`/product/${product}`}>
-            <button type="button">{buttonText}</button>
+            <button type="button" className="footer-button">
+              {buttonText}
+            </button>
           </Link>
         </div>
-        <img src={urlFor(image)} alt="" className="footer-banner-image" />
       </div>
     </div>
   );
