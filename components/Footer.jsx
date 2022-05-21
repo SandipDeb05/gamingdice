bc84ee695cb9d884ae6737be90c91f2d74f05833
SandipDeb <sandip.deb@xopuntech.com>
2022-05-21 22:05:37 +0530

    final check for target blank

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,10 +7,18 @@ const Footer = () => {
       <p>Sandip Store 2022 || All rights reserved</p>
       <p>Coded by Sandip Deb</p>
       <p className="icons">
-        <a href="https://www.linkedin.com/in/sandip-deb-8b76b2157/">
+        <a
+          href="https://www.linkedin.com/in/sandip-deb-8b76b2157/"
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+        >
           <AiFillLinkedin />
         </a>
-        <a href="https://github.com/SandipDeb05">
+        <a
+          href="https://github.com/SandipDeb05"
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+        >
           <AiFillGithub />
         </a>
       </p>
